337a9ce8d274f7d266881404bdcbdf23e3076b49
walz0 <aidan.m.walz@gmail.com>
2020-03-01 22:13:28 -0500

    Added todo.py

--- todo.py
+++ todo.py
@@ -5,21 +5,40 @@ courses = [
 	"CSCI-C200"  
 ]
 
+def containsCourse(str):
+	for c in courses:
+		if c in str:
+			return True
+	return False
+	
+
 with open ("TODO.txt", "r") as todo:
 	rawTodo = todo.read().strip()
 
 lines = rawTodo.splitlines()
 
-for c in courses:
-	for l in lines:
-		if c in l:
-			print(c)
+assignment = []
+sub = []
+complete = []
 
 for l in lines:
-	for i in l:
+	if len(l) > 0:
+		# Complete Assignment Detected
+		if l.strip()[0] == "/":
+			complete.append(l.strip())
 		# Assignment Detected
-		if i == "-":
-			print(l.strip())
+		elif l.strip()[0] == "-" and not containsCourse(l):
+			assignment.append(l.strip())
 		# Assignment Part
-		elif i == ".":
-			print(l.strip())
+		elif l.strip()[0] == ".":
+			sub.append(l.strip())
+
+print('Assignments:')
+for a in assignment:
+	print(a)
+print('Sub:')
+for s in sub:
+	print(s)
+print('Completed:')
+for c in complete:
+	print(c)
